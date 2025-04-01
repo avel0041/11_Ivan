d1a21070a36198e4cb9d2d7314f0e2c6b222545d
avel0041 <vel-ar@mail.ru>
2025-04-01 19:01:50 +0300

    24

--- EGE/24/24_0.py
+++ EGE/24/24_0.py
@@ -2,3 +2,21 @@
 # Определите максимальное количество идущих подряд символов C.
 
 f = open('24_0.txt')
+l = f.readline()
+c = 1 # с 1 из за 'ACACACACACA'
+c1 = 0
+for i in range(0, len(l) - 1, 1):
+    if l[i] == 'C':
+        if l[i] == l[i + 1]:
+            c += 1
+            if c > c1:
+                c1 = c
+    else:
+        c = 1
+print(c1)
+
+
+
+
+
+    
